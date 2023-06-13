ea00a7243686e1a47b48bb2c5ed07b16cdc2fd72
ByCybeR <cyber.pra6@gmail.com>
2023-06-13 12:58:28 -0400

    Casi Hecho

--- cne-game/Assets/Scripts/ballMove.cs
+++ cne-game/Assets/Scripts/ballMove.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ballMove : MonoBehaviour
-    
-
-
 {
+    public float speed;
     public Vector3 initialImpulse;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(initialImpulse, ForceMode.Impulse);
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(initialImpulse, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = rb.velocity.normalized * speed;
     }
 }
