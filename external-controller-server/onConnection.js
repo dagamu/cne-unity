f091e330876d6fdcd2ca3ad0fb600d3bb5f7cf96
dagamu <gajardomunoz@gmail.com>
2023-10-23 20:47:57 -0300

    Server and Animation Update

---
+++ external-controller-server/onConnection.js
@@ -0,0 +1,83 @@
+const Colors = [
+    [0,0,1,0],  //Blue
+    [0,1,0,0],  //Green
+    [1,1,0,0],  //Yellow
+    [1,0,0,0]   //Red
+]
+
+const colorStr = [ "blue", "green", "yellow", "red" ]
+
+var connections = []
+
+module.exports = function(ws, newLog, wss) {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //console.log(message.toString())
+
+        if(message == 'createPlayer'){
+
+            let newId = getUniqueID()  
+            ws.id = newId
+            
+            let colorIndex = 0
+            ws.color = Colors[colorIndex].join(';')
+            while( connections.filter( c => c.color == ws.color ).length ){
+                colorIndex++
+                ws.color = Colors[colorIndex].join(';')
+            }
+
+            ws.send("ID:"+ws.id);
+            ws.send("Color:"+ws.color)
+
+            wss.clients.forEach(c => c.send(`newPlayer:${newId},${ ws.color }`))
+            
+            connections.push(ws)
+
+            newLog(`New Player entered: ${newId} [${connections.length}]`, colorStr[colorIndex])
+
+        } else if( message == "UnityConnection"){
+            
+            let newId = getUniqueID() 
+            ws.id = newId
+            ws.isUnityClient = true
+            unityClient = ws.id
+
+            newLog(`UnityClient established: ${newId}`)
+
+            if( connections.length == 0 ){
+                newId = getUniqueID() 
+                devPlayerBot = true;
+                newLog(`Dev player bot established: ${newId}`)
+                connections.push({id:newId, color:Colors[0].join(';')})
+            }
+
+            connections.forEach( p => {
+                newLog(`Player ${ p.id } entered into Unity`)
+                ws.send(`newPlayer:${ p.id },${ p.color }`)
+            })
+        }
+
+        wss.clients.forEach(c => 
+            c.send(`${message}`))
+    });
+
+    ws.on('close', () => {
+        if( ws.isUnityClient ) { 
+            console.log(`UnityClient left: ${ws.id}`)
+            if(devPlayerBot ){
+                var bot = connections.shift()
+                devPlayerBot = false
+                newLog(`Dev player bot killed: ${bot.id}`)
+            }
+            return
+        }
+        connections.splice(connections.indexOf( ws ), 1)
+        newLog(`Player left: ${ws.id} [${connections.length}]`)
+        wss.clients.forEach( c => {
+            c.send('Delete:' + ws.id)
+            
+        })
+    })
+}
